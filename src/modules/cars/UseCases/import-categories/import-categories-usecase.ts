50febadb5bfb3f00d771a22ee451101f9a766cc5
MatheusFontenele <matheus.fontenele@icloud.com>
2022-01-30 21:38:10 -0300

    feat: add csv-parser and ensure read file csv to format datas

--- src/modules/cars/UseCases/import-categories/import-categories-usecase.ts
+++ src/modules/cars/UseCases/import-categories/import-categories-usecase.ts
@@ -1,5 +1,12 @@
+import fs from 'fs'
+import { parse } from 'csv-parse'
 export class ImportCategoryUseCase {
   execute(file: Express.Multer.File) {
-    console.log(file);
+    const stream = fs.createReadStream(file.path)
+    const parser = parse();
+    stream.pipe(parser)
+    parser.on("data", async (line) => {
+      console.log(line);
+    })
   }
 }
