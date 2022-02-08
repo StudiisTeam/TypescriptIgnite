9ad91d3507afb946a1a902a32ecff03e25873382
MatheusFontenele <matheus.fontenele@icloud.com>
2022-02-07 21:50:34 -0300

    feat: ensure create migration of creation categories

--- src/database/migrations/1644280491534-CreateCategories.ts
+++ src/database/migrations/1644280491534-CreateCategories.ts
@@ -1,11 +1,38 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateCategories1644280491534 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true
+          },
+          {
+            name: "name",
+            type: "varchar"
+          },
+          {
+            name: "description",
+            type: "varchar"
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now("
+          }
+        ]
+      })
+    )
+
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('categories')
   }
 
 }
