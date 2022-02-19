8597240ddacc26e98b971d3d4ef090ce8672bab8
MatheusFontenele <matheus.fontenele@icloud.com>
2022-02-19 13:47:30 -0300

    test: ensure createCategory call create with a correct values

---
+++ src/modules/cars/UseCases/create-categories/create-category-usecase.spec.ts
@@ -0,0 +1,24 @@
+import { CategoryRepositoryInMemory } from "../../repositories/in-memory/categories-repository-in-memory";
+import { CreateCategoryUseCase } from "./create-category-usecase";
+
+let categoriesRepository: CategoryRepositoryInMemory
+let createCategoryUseCase: CreateCategoryUseCase
+
+describe('Create Category', () => {
+
+  beforeEach(() => {
+    categoriesRepository = new CategoryRepositoryInMemory()
+    createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository)
+  })
+
+  test('should first', async () => {
+    const category = {
+      name: "any_Category",
+      description: "any_descritption"
+    }
+    await createCategoryUseCase.create(category)
+
+    const categoryCreated = await categoriesRepository.findByName(category.name)
+    expect(categoryCreated).toHaveProperty("id")
+  })
+});
