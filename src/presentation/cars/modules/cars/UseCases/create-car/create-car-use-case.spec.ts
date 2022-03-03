f21dfed320df33432710816193b0e773e635a394
MatheusFontenele <matheus.fontenele@icloud.com>
2022-03-03 08:58:51 -0300

    test: ensure CreateCar be able to create a new car

--- src/presentation/cars/modules/cars/UseCases/create-car/create-car-use-case.spec.ts
+++ src/presentation/cars/modules/cars/UseCases/create-car/create-car-use-case.spec.ts
@@ -1,76 +1,77 @@
 import { AppError } from "../../../../../../errors/app-erros";
 import { ICreateCar } from "../../dtos/create-car";
-import { Car } from "../../entities/cars";
-import { CarRepository } from "../../repositories/implemetations/car-repository";
+import { Cars } from "../../entities/cars";
+import { ICarRepository } from "../../repositories/car-repository-protocols";
 import { CreateCarUseCase } from "./create-car-use-case";
 
 interface SutTypes {
-  sut: CreateCarUseCase
-  carRepositoryStub: CarRepository
+  sut: CreateCarUseCase;
+  carRepositoryStub: ICarRepository;
 }
 
 const makeFakeCar = () => ({
-  "available": true,
-  "brand": "any_brand",
-  "category_id": "any_category",
-  "daily_rate": 100,
-  "description": "any_description",
-  "fine_amount": 10,
-  "id": "valid_id",
-  "license_plate": "any_license",
-  "name": "any_name",
-})
+  available: true,
+  brand: "any_brand",
+  category_id: "any_category",
+  daily_rate: 100,
+  description: "any_description",
+  fine_amount: 10,
+  id: "valid_id",
+  license_plate: "any_license",
+  name: "any_name",
+});
 
 const makeCarRepository = () => {
-  class CarRepositoryStub implements CarRepository {
-    cars: Car[] = []
-    async add(carData: ICreateCar): Promise<Car> {
-      const car = new Car()
-      Object.assign(car, carData)
-      this.cars.push(car)
-      return new Promise((resolve, reject) => resolve(makeFakeCar()))
+  class CarRepositoryStub implements ICarRepository {
+    cars: Cars[] = [];
+    async add(carData: ICreateCar): Promise<Cars> {
+      const car = new Cars();
+      Object.assign(car, carData);
+      this.cars.push(car);
+      return new Promise((resolve, reject) => resolve(makeFakeCar()));
     }
-    async findCarByLicensePlate(license_plate: string): Promise<Car> {
-      const car = await this.cars.find((car) => car.license_plate === license_plate)
-      return car
+    async findCarByLicensePlate(license_plate: string): Promise<Cars> {
+      const car = await this.cars.find(
+        (car) => car.license_plate === license_plate
+      );
+      return car;
     }
   }
-  return new CarRepositoryStub()
-}
+  return new CarRepositoryStub();
+};
 
 const makeSut = (): SutTypes => {
-  const carRepositoryStub = makeCarRepository()
-  const sut = new CreateCarUseCase(carRepositoryStub)
+  const carRepositoryStub = makeCarRepository();
+  const sut = new CreateCarUseCase(carRepositoryStub);
   return {
     sut,
-    carRepositoryStub
-  }
-}
+    carRepositoryStub,
+  };
+};
 
 const makeFakeRequest = () => ({
-  name: 'any_name',
-  description: 'any_description',
+  name: "any_name",
+  description: "any_description",
   daily_rate: 100,
-  license_plate: 'any_license',
+  license_plate: "any_license",
   available: true,
   fine_amount: 10,
-  brand: 'any_brand',
-  category_id: 'any_category',
-})
+  brand: "any_brand",
+  category_id: "any_category",
+});
 
-describe('Create Car', () => {
-
-  test('should be able to create a new car', async () => {
-    const { sut } = makeSut()
-    const car = await sut.add(makeFakeRequest())
-    expect(car).toEqual(makeFakeCar())
+describe("Create Car", () => {
+  test("should be able to create a new car", async () => {
+    const { sut } = makeSut();
+    const car = await sut.add(makeFakeRequest());
+    expect(car).toEqual(makeFakeCar());
   });
 
-  test('should not be able to create a car if exist a licence plate', () => {
-    const { sut } = makeSut()
+  test("should not be able to create a car if exist a licence plate", () => {
+    const { sut } = makeSut();
     expect(async () => {
-      await sut.add(makeFakeRequest())
-      await sut.add(makeFakeRequest())
-    }).rejects.toBeInstanceOf(AppError)
+      await sut.add(makeFakeRequest());
+      await sut.add(makeFakeRequest());
+    }).rejects.toBeInstanceOf(AppError);
   });
-})
+});
