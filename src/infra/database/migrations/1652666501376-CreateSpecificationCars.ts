c06c52869d27af91a95df9bf84dd2027d6c020d7
MatheusFontenele <matheus.fontenele@icloud.com>
2022-10-24 19:52:45 -0300

    bugfix(migrations): corrige erro ao executar as migracoes

--- src/infra/database/migrations/1652666501376-CreateSpecificationCars.ts
+++ src/infra/database/migrations/1652666501376-CreateSpecificationCars.ts
@@ -1,69 +1,58 @@
 import {
-  MigrationInterface,
-  QueryRunner,
-  Table,
-  TableForeignKey,
+    MigrationInterface,
+    QueryRunner,
+    Table,
+    TableForeignKey,
 } from "typeorm";
 
 export class CreateSpecificationCars1652666501376
-  implements MigrationInterface
-{
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.createTable(
-      new Table({
-        name: "specification_car",
-        columns: [
-          {
-            name: "car_id",
-            type: "uuid",
-          },
-          {
-            name: "specification_id",
-            type: "uuid",
-          },
-          {
-            name: "created_at",
-            type: "timestamp",
-            default: "now()",
-          },
-        ],
-      })
-    );
+    implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "specification_car",
+                columns: [
+                    { name: "car_id", type: "uuid" },
+                    { name: "specification_id", type: "uuid" },
+                    { name: "created_at", type: "timestamp", default: "now()" },
+                ],
+            })
+        );
 
-    await queryRunner.createForeignKey(
-      "specification_cars",
-      new TableForeignKey({
-        name: "FKSpecificationCar",
-        referencedTableName: "specification",
-        referencedColumnNames: ["id"],
-        columnNames: ["specification_id"],
-        onDelete: "SET NULL",
-        onUpdate: "SET NULL",
-      })
-    );
-    await queryRunner.createForeignKey(
-      "specification_cars",
-      new TableForeignKey({
-        name: "FKCarSpecification",
-        referencedTableName: "cars",
-        referencedColumnNames: ["id"],
-        columnNames: ["car_id"],
-        onDelete: "SET NULL",
-        onUpdate: "SET NULL",
-      })
-    );
-  }
+        await queryRunner.createForeignKey(
+            "specification_car",
+            new TableForeignKey({
+                name: "FKSpecificationCar",
+                referencedTableName: "specification",
+                referencedColumnNames: ["id"],
+                columnNames: ["specification_id"],
+                onDelete: "SET NULL",
+                onUpdate: "SET NULL",
+            })
+        );
+        await queryRunner.createForeignKey(
+            "specification_car",
+            new TableForeignKey({
+                name: "FKCarSpecification",
+                referencedTableName: "cars",
+                referencedColumnNames: ["id"],
+                columnNames: ["car_id"],
+                onDelete: "SET NULL",
+                onUpdate: "SET NULL",
+            })
+        );
+    }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropForeignKey(
-      "specification_cars",
-      "FKCarSpecification"
-    );
-    await queryRunner.dropForeignKey(
-      "specification_cars",
-      "FKSpecificationCar"
-    );
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey(
+            "specification_car",
+            "FKCarSpecification"
+        );
+        await queryRunner.dropForeignKey(
+            "specification_car",
+            "FKSpecificationCar"
+        );
 
-    await queryRunner.dropTable("specification_car");
-  }
+        await queryRunner.dropTable("specification_car");
+    }
 }
